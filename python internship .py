9faa248675237a6408dd630b143139695534c448
mabdullah49 <mabdullah31103@gmail.com>
2025-06-24 14:21:42 +0500

    Create python internship .py

---
+++ python internship .py
@@ -0,0 +1,12 @@
+# Day 1 
+# Task: Intro to Variables & Typecasting
+
+
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = input("Enter a string: ")
+
+print("\n--- Output ---")
+print(f"Integer value: {int_input}")
+print(f"Float value: {float_input}")
+print(f"String value: {string_input}")
