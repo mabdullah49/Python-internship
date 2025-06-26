8b231e85d6402a93cfc6fa6f1c3c00205b07e1aa
mabdullah49 <mabdullah31103@gmail.com>
2025-06-26 13:12:03 +0500

    Create Day 3 .py

---
+++ Day 3 .py
@@ -0,0 +1,14 @@
+
+sentence = input("Enter a sentence: ")
+
+words_list = sentence.split()
+
+print("List of words:", words_list)
+
+joined_sentence = ' - '.join(words_list)
+print("Joined sentence:", joined_sentence)
+
+name_tuple = ("Mihammad", "Abdullah")
+
+print("First Name:", name_tuple[0])
+print("Last Name:", name_tuple[1])
